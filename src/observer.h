c382ec69152785827c2588589ebff74aaadaaa6a
ShelpAm <shelpam@qq.com>
2024-07-10 20:23:58 +0800

    refactor(observer.h): rename class names and varible names

--- src/observer.h
+++ src/observer.h
@@ -1,39 +1,41 @@
 #pragma once
+#include <algorithm>
 #include <iostream>
 #include <memory>
 #include <vector>
 
-class Subscriber {
+class Observer {
 public:
-  virtual ~Subscriber() = default;
+  virtual ~Observer() = default;
+
   virtual void update() = 0;
 };
 
-class Publisher {
+class Subject {
 public:
-  void notify_all() const {
-    for (auto const &subscriber : _subscribers) {
-      if (auto const ptr{subscriber.lock()}; ptr) {
+  void notify() const {
+    std::ranges::for_each(_observers, [](auto const &weak_observer) {
+      if (auto const ptr{weak_observer.lock()}; ptr) {
         ptr->update();
       }
-    }
+    });
   }
 
-  void subscribe(std::shared_ptr<Subscriber> const &subscriber) {
-    _subscribers.push_back(subscriber);
+  void attach(std::shared_ptr<Observer> const &observer) {
+    _observers.push_back(observer);
   }
 
-  void unsubscribe(std::shared_ptr<Subscriber> const &subscriber) {
-    std::erase_if(_subscribers, [&subscriber](auto const &wp) {
-      return wp.expired() || wp.lock() == subscriber;
+  void detach(std::shared_ptr<Observer> const &observer) {
+    std::erase_if(_observers, [&observer](auto const &wp) {
+      return wp.expired() || wp.lock() == observer;
     });
   }
 
 private:
-  std::vector<std::weak_ptr<Subscriber>> _subscribers;
+  std::vector<std::weak_ptr<Observer>> _observers;
 };
 
-class Unhappy_to_observe_subject : public Subscriber {
+class Unhappy_to_observe_subject : public Observer {
 public:
   void update() override { std::cout << "Unhappy to observe subject...\n"; }
 };
